52d3dd8073477276813e8144963c1ed31ff1d333
Crowdbotics <team@crowdbotics.com>
2020-05-10 20:40:57 +0000

    Initial commit.

---
+++ backend/can_you_understand_16842/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for can_you_understand_16842 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'can_you_understand_16842.settings')
+
+application = get_wsgi_application()
